a7538baeda306a11b066f8fd6e9f5c2a303f5513
Shapin199 <mdtanvir054321@gmail.com>
2025-02-02 02:32:45 +0600

    ProductId add unique

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import logo from '././images/favicon.ico'
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import Review from './components/Review/Review';
+import Notfound from './components/NotFound/Notfound';
+import ProductDetails from './components/ProductDetails/ProductDetails';
 
 function App() {
   return (
-    <div >
-      <Header></Header>    
-      <Shop></Shop>
-    </div>
+    <Router>
+      <Header />  
+      <Routes>
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/review" element={<Review></Review>} />
+        <Route path="/manage" element={<h1>This is manage</h1>} />
+        <Route path="/" element={<Shop />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<Notfound></Notfound>} />
+      </Routes>
+    </Router>
   );
 }
 
